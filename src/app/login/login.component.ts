c3f70b2ac263cef8b0571666ab8cf43f22d88edf
omarsalem7 <omarsalem0721@gmail.com>
2022-05-18 21:35:44 +0200

    Add routes for E-learning and add login and signup pages

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../shared/login_signup.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
